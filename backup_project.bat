eda4b5fbb016717d428150a38c4f8495a789ce70
QUIZZY04 <madhhu52@gmail.com>
2026-03-10 15:26:02 +0530

    Update backup_project.bat

--- backup_project.bat
+++ backup_project.bat
@@ -1,1 +1,12 @@
 
+@echo off
+
+set folder=backup\backup_%date:~-4%-%date:~4,2%-%date:~7,2%_%time:~0,2%%time:~3,2%
+
+mkdir %folder%
+
+xcopy * %folder% /E /I /Y
+
+echo Backup completed successfully!
+
+pause
